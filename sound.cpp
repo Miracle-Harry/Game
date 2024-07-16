c46f5daeb59d010b158fe2c7dae7027387b28c2a
Miracle-Harry <harryszb0017@gmail.com>
2024-07-16 16:12:04 +0900

    BGM

--- sound.cpp
+++ sound.cpp
@@ -33,8 +33,8 @@ DWORD g_aSizeAudio[SOUND_LABEL_MAX] = {};					// オーディオデータサイズ
 SOUNDPARAM g_aParam[SOUND_LABEL_MAX] =
 {
 	{ (char*)"data/BGM/sample000.wav", -1 },	// BGM0
-	{ (char*)"data/BGM/bgm_maoudamashii_neorock73.wav", -1 },	// BGM1
-	{ (char*)"data/BGM/sample001.wav", -1 },	// BGM2
+	{ (char*)"data/BGM/RPG_03.wav", -1 },	// BGM1
+	{ (char*)"data/BGM/RPG_02.wav", -1 },	// BGM2
 	{ (char*)"data/SE/bomb000.wav", 0 },		// 弾発射音
 	{ (char*)"data/SE/defend000.wav", 0 },		// 弾発射音
 	{ (char*)"data/SE/defend001.wav", 0 },		// 弾発射音
@@ -44,7 +44,7 @@ SOUNDPARAM g_aParam[SOUND_LABEL_MAX] =
 	{ (char*)"data/SE/shot000.wav", 0 },		// 弾発射音
 	{ (char*)"data/SE/shot001.wav", 0 },		// ヒット音
 
-	{ (char*)"data/BGM/maou.wav", -1 },			// BGM Maou
+	{ (char*)"data/BGM/RPG_01.wav", -1 },			// BGM Maou
 
 };
 
